ae4ed16c52ccbf9215d0cf36895edaf25e0f5a8d
Davide Guidetti <73661177+ilmada99@users.noreply.github.com>
2022-04-09 10:12:57 +0200

    Create TablutDalo.java

---
+++ TablutDalo2022/src/it/unibo/ai/didattica/competition/tablut/client/TablutDalo.java
@@ -0,0 +1,143 @@
+package it.unibo.ai.didattica.competition.tablut.client;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.security.InvalidParameterException;
+
+import it.unibo.ai.didattica.competition.tablut.domain.Action;
+import it.unibo.ai.didattica.competition.tablut.domain.Game;
+import it.unibo.ai.didattica.competition.tablut.domain.GameAshtonTablut;
+import it.unibo.ai.didattica.competition.tablut.domain.State;
+import it.unibo.ai.didattica.competition.tablut.domain.StateTablut;
+import it.unibo.ai.didattica.competition.tablut.domain.State.Turn;
+import it.unibo.ai.didattica.competition.tablut.exceptions.ActionException;
+import search.MinMaxSearch;
+import search.SearchStrategy;
+
+public class TablutDalo extends TablutClient {
+	public static final String NAME = "Cliente";
+	public static final int DEPTH = 4;
+	private SearchStrategy searchStrategy;
+
+	public TablutDalo(String player, String name, int timeout, String ipAddress)
+			throws UnknownHostException, IOException {
+		super(player, name, timeout, ipAddress);
+		Game rules = new GameAshtonTablut(this.getCurrentState(), 2, 2, "log", "White", "Black");
+        searchStrategy = new MinMaxSearch(rules,DEPTH);
+
+	}
+
+	public static void main(String[] args) {
+		String role;
+		int timeout;
+		String ipAddress;
+		if (args.length != 3) {
+			System.out.println("Errore args: <role> <timeout> <ipAddress>");
+			System.out.println("Default Setting:\nrole: white\ntimeout=5 sec\nipAddress=\"Localhost\"");
+			role = "white";
+			timeout = 5;
+			ipAddress="localhost";
+		} else {
+			role = args[0];
+			if (!role.equalsIgnoreCase("white") && !role.equalsIgnoreCase("black")) {
+				System.out.println("Player role must be BLACK or WHITE");
+				System.exit(1);
+			}
+			timeout = 0;
+			try {
+				timeout = Integer.parseInt(args[1]);
+			} catch (NumberFormatException e) {
+				System.out.println("The timeout has to be an integer");
+				System.exit(1);
+			}
+			ipAddress = args[2];
+		}
+		
+		System.out.println("Connecting to the server...");
+		TablutDalo client = null;
+		try {
+			client = new TablutDalo(role, NAME, timeout, ipAddress);
+		} catch (InvalidParameterException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + ipAddress);
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Error connecting to the server: " + e.getMessage());
+			System.exit(1);
+		}
+		System.out.println("Connected");
+		client.run();
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println("Declaring name...");
+			this.declareName();
+		} catch (Exception e) {
+			System.out.println("Error declaring name: " + e.getMessage());
+			System.exit(1);
+		}
+
+		State state;
+
+		System.out.println("You are player " + this.getPlayer());
+		System.out.println("Start of the match!");
+
+		while (true) {
+			try {
+				this.read(); //reading JSON from server
+			} catch (ClassNotFoundException | IOException e1) {
+				System.out.println("Error reading the state: " + e1.getMessage());
+				System.exit(1);
+			}
+
+			state = this.getCurrentState();
+			// check if the game is finish 
+			
+			// I won
+			if (this.getPlayer().equals(State.Turn.WHITE) && state.getTurn().equals(State.Turn.WHITEWIN)
+					|| this.getPlayer().equals(State.Turn.BLACK) && state.getTurn().equals(State.Turn.BLACKWIN)) {
+				System.out.println("YOU WIN!");
+				System.exit(0);
+			}
+			// I lose
+			else if (this.getPlayer().equals(State.Turn.WHITE) && state.getTurn().equals(State.Turn.BLACKWIN)
+					|| this.getPlayer().equals(State.Turn.BLACK) && state.getTurn().equals(State.Turn.WHITEWIN)) {
+				System.out.println("YOU LOSE!");
+				System.exit(0);
+			}
+			// Draw
+			else if (state.getTurn().equals(StateTablut.Turn.DRAW)) {
+				System.out.println("DRAW!");
+				System.exit(0);
+			}
+			
+			// My turn
+			else if (this.getPlayer().equals(this.getCurrentState().getTurn())) {
+				Action chosenMove = null;
+				try {
+					System.out.println("I am thinking...");
+					chosenMove = this.searchStrategy.choseMove(state,this.getPlayer().toString());
+					System.out.println("Chosen move: " + chosenMove);
+
+				} catch (IOException | ActionException e) {
+					System.out.println("Error choosing the move: " + e.getMessage());
+				}
+				try {
+					this.write(chosenMove);
+				} catch (IOException | ClassNotFoundException e) {
+					System.out.println("Error sending the move: " + e.getMessage());
+					System.exit(1);
+				}
+			}
+			// Adversary turn
+			else {
+				System.out.println("Waiting for your opponent move...");
+			}
+		}
+
+	}
+}
